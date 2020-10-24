142cc54a3f4a8ae707d7cbb2a89ab4c0bc00b1c2
NatrezC <cnatrez@gmail.com>
2020-10-23 20:39:33 -0500

    installed axios

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+require('dotenv').config()
+const { default: Axios } = require('axios')
 const express = require('express')
 const ejsLayouts = require('express-ejs-layouts')
 const app = express()
+const axios = require('axios')
 
 app.set('view engine', "ejs")
 app.use(ejsLayouts)
 
-app.get('/', (req, res)=>{
-    res.render('home')
+//---->Movies index route
+app.get('/movies', (req, res)=>{
+    axios.get(`http://www.omdbapi.com/?apikey=${process.emitWarning.API_KEY}`)
+    res.render('index')
 })
 
 app.listen(7000, ()=>{
